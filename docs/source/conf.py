a410dec17a3d16dc44ede7e0b19213c26607d4f4
Ninh Bui Gia <bg.ninh@gmail.com>
2024-08-01 23:25:44 +0200

    First commit.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-project = 'Custom TREND Plotter'
+project = 'customTRENDplotter'
 copyright = '2024, Ninh Bui Gia'
 author = 'Ninh Bui Gia'
 release = '0.0.1'
